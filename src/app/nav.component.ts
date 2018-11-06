1b286fd6a0ed7807efcc0fa3ea3e773081673a89
Karan Sharma <sharma.karan9341@gmail.com>
2018-11-06 11:34:27 -0500

    Made changes to Nav

--- src/app/nav.component.ts
+++ src/app/nav.component.ts
@@ -8,12 +8,11 @@ import { Router } from '@angular/router';
     selector: 'app-nav',
     template: `
         <mat-toolbar color="primary">
-            <button mat-button (click)="display()">RTDV</button>
+            <button mat-button (click)="display()">{{disAppName}}</button>
             <button *ngIf="authentication.isAuthenticated" mat-button routerLink="/dtvizmessages">DTMS</button>
             <span style="flex: 1 1 auto"></span>
             <button *ngIf="!authentication.isAuthenticated" mat-button routerLink="/login">Login</button>
             <button *ngIf="!authentication.isAuthenticated" mat-button routerLink="/register">Register</button>
-            <button *ngIf="authentication.isAuthenticated" mat-button routerLink="/dashboard">Welcome {{authentication.name}}</button>
             <button *ngIf="authentication.isAuthenticated" mat-button (click)="authentication.logout()">Logout</button>
         </mat-toolbar>
         
@@ -22,15 +21,17 @@ import { Router } from '@angular/router';
 
 export class NavComponent {
     constructor(public authentication : AuthenticationService, private router : Router) {}
-
+    disAppName="RTDV";
     display(){
         console.log('DISPLAY_ME');
         var _router = this.router;
         if(this.authentication.isAuthenticated){
             console.log('AUTH');
+            this.disAppName = 'RTDV Welcome' + this.authentication.name;
             _router.navigateByUrl('/dashboard');
         }else{
             console.log('NOT_AUTH');
+            
             _router.navigateByUrl('/');
         }
     }
